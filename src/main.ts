e8cc6e248ea8efc6b31a0ac715b2e1b0d96d3b72
libterty <albert@ohbottech.com>
2020-03-15 14:06:26 +0800

    main.ts setup and config file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe, Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { config } from '../config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const validationOptions = {
+    skipMissingProperties: true,
+    validationError: { target: false },
+  };
+  app.useGlobalPipes(new ValidationPipe(validationOptions));
+  app.setGlobalPrefix(config.PREFIX);
+  app.enableCors({ origin: true });
+  await app.listen(config.PORT, config.HOST);
+  Logger.log(`Server listening on port ${config.PORT}`, 'Bootstrap');
 }
 bootstrap();
